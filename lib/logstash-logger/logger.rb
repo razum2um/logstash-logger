730dac5592c52cd25ed6fa603e0350f922d06b20
Vlad Bokov <razum2um@mail.ru>
2013-07-23 17:34:52 +0700

    bump logstash event format

--- lib/logstash-logger/logger.rb
+++ lib/logstash-logger/logger.rb
@@ -2,8 +2,7 @@ class LogStashLogger < ::Logger
   
   attr_reader :client
   
-  LOGSTASH_EVENT_FIELDS = %w(@timestamp @tags @type @source @fields @message).freeze
-  HOST = ::Socket.gethostname
+  LOGSTASH_EVENT_FIELDS = %w(@timestamp @version).freeze
   
   def initialize(host, port, socket_type=:udp)
     super(::LogStashLogger::Socket.new(host, port, socket_type))
@@ -14,14 +13,8 @@ class LogStashLogger < ::Logger
     if severity < @level
       return true
     end
-    progname ||= @progname
-    if message.nil?
-      if block_given?
-        message = yield
-      else
-        message = progname
-        progname = @progname
-      end
+    if message.nil? and block_given?
+      message = yield
     end
     @logdev.write(
       format_message(format_severity(severity), LogStash::Time.now, progname, message))
@@ -33,33 +26,22 @@ class LogStashLogger < ::Logger
     if data.is_a?(String) && data[0] == '{'
       data = (JSON.parse(message) rescue nil) || message
     end
-    
+
     event = case data
     when LogStash::Event
       data.clone
     when Hash
       event_data = {
-        "@tags" => [],
-        "@fields" => {},
-        "@timestamp" => time
+        "@timestamp" => time,
+        "@version" => 1
       }
-      LOGSTASH_EVENT_FIELDS.each do |field_name|
-        if field_data = data.delete(field_name)
-          event_data[field_name] = field_data
-        end
-      end
-      event_data["@fields"].merge!(data)
+      event_data.merge!(data)
       LogStash::Event.new(event_data)
     when String
-      LogStash::Event.new("@message" => data, "@timestamp" => time)
+      LogStash::Event.new("message" => data, "@timestamp" => time, '@version' => 1)
     end
-    
+
     event['severity'] ||= severity
-    #event.type = progname
-    if event.source == 'unknown'
-      event["@source"] = HOST
-    end
-    
     event
   end
 end
